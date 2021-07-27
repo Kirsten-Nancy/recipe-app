106460e3656981c80b2735bc7ad6d870fc90b9c7
Kirsten Nancy <korjnan@gmail.com>
2021-07-27 13:27:16 +0800

    modify fetch data functionality

--- src/recipe/FoodList.js
+++ src/recipe/FoodList.js
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react"
 import Food from "./Food"
 
-const FoodList = ({ data }) => {
+const FoodList = () => {
+  const [data, setData] = useState([])
+
+  const fetchRecipes = async () => {
+    let response = await fetch("http://127.0.0.1:8000/api/recipes/", {
+      headers: {
+        Accept: "application/json;",
+      },
+    })
+    let api_data = await response.json()
+    setData(api_data)
+  }
+
+  useEffect(() => {
+    fetchRecipes()
+  }, [])
   return (
     <div className="main-container">
-      {/* {JSON.stringify(data)} */}
       {data.map((recipe, index) => {
         return <Food key={index} recipe={recipe} />
       })}
