c95e1646b2d093214b6f58181c9279ccbff8d24a
Kirsten Nancy <korjnan@gmail.com>
2021-07-27 13:28:30 +0800

    Add edit and delete btns

--- src/recipe/Food.js
+++ src/recipe/Food.js
@@ -1,22 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { FaEdit, FaTrash } from "react-icons/fa"
 
 const Food = ({ recipe }) => {
   return (
-    <Link
-      to={{
-        pathname: `/recipes/${recipe.id}`,
-      }}
-    >
-      <div className="card">
+    <div className="card">
+      <Link
+        to={{
+          pathname: `/recipes/${recipe.id}`,
+        }}
+      >
         <img
           src="https://img.taste.com.au/e8zdceW_/w643-h428-cfill-q90/taste/2016/11/zucchini-slice-10160-1.jpeg"
           alt=""
         />
         <h5>{recipe?.title}</h5>
         <p>{recipe?.description}</p>
-      </div>
-    </Link>
+      </Link>
+      <Link
+        to={{
+          pathname: `/update/${recipe.id}`,
+        }}
+      >
+        {" "}
+        <FaEdit />
+      </Link>
+
+      <FaTrash />
+    </div>
   )
 }
 
