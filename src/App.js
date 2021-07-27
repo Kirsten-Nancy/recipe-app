dc273b2cb067ab84705524828796b38c752375b9
Kirsten Nancy <korjnan@gmail.com>
2021-07-27 13:29:25 +0800

    Add update route

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React from "react"
-import { useState, useEffect } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Nav from "./Nav"
 import FoodList from "./recipe/FoodList"
 import AddRecipeForm from "./recipe/components/AddRecipeForm"
 import FoodDetail from "./recipe/FoodDetail"
 import Register from "./authentication/Register"
+import RecipeUpdate from "./recipe/RecipeUpdate"
 
 const App = () => {
-  const [data, setData] = useState([])
-
-  const fetchRecipes = async () => {
-    let response = await fetch("http://127.0.0.1:8000/api/recipes/", {
-      headers: {
-        Accept: "application/json; odata=verbose",
-      },
-    })
-    let api_data = await response.json()
-    setData(api_data)
-  }
-
-  useEffect(() => {
-    fetchRecipes()
-  }, [])
-
   return (
     <Router>
       <div className="app">
         <Nav />
         <Switch>
           <Route path="/" exact>
-            {data && <FoodList data={data} />}
+            <FoodList />
           </Route>
-          <Route path="/create">
-            <AddRecipeForm data={data} setData={setData} />
-          </Route>
+          <Route path="/create" component={AddRecipeForm} />
+          <Route path="/update/:id" component={RecipeUpdate} />
           <Route path="/recipes/:id" component={FoodDetail} />
           <Route path="/register" component={Register} />
         </Switch>
