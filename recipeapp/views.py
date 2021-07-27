95685faacc6c02c468daf251e3b31b351d435b8c
Kirsten Nancy <korjnan@gmail.com>
2021-07-27 13:23:40 +0800

    add put and delete functions

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -6,29 +6,51 @@ from .models import Recipe
 from .serializers import RecipeSerializer
 
 
-@api_view(['GET'])
-def get_recipes(request):
-    recipes = Recipe.objects.all()
-    serializer = RecipeSerializer(recipes, many=True)
+@api_view(['GET', 'POST'])
+def recipe_list(request):
+    if request.method == 'GET':
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        print('request data', request.data)
+        serializer = RecipeSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # The arg name passed should be similar to that in the urls file
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def recipe_detail(request, recipe_id):
-    recipe = Recipe.objects.get(pk=recipe_id)
-    serializer = RecipeSerializer(recipe, many=False)
 
-    return Response(serializer.data)
+    try:
+        recipe = Recipe.objects.get(pk=recipe_id)
+    except Recipe.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET':
+        # The serializer by default expects only one object,
+        # No need to pass the many parameter
+        serializer = RecipeSerializer(recipe)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def create_recipe(request):
-    print('request data', request.data)
-    serializer = RecipeSerializer(data=request.data)
+    elif request.method == 'PUT':
+        # When you are updating a particular entry, you pass the instance of that object and the data to update
+        serializer = RecipeSerializer(instance=recipe, data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
