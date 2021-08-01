864f6cfcfe30123c1dfe280150119c75098d4403
Kirsten Nancy <korjnan@gmail.com>
2021-08-01 18:42:38 +0800

    Overwritten create and update methods

--- recipeapp/serializers.py
+++ recipeapp/serializers.py
@@ -4,17 +4,22 @@ from rest_framework import serializers, fields
 
 class IngredientSerializer(serializers.ModelSerializer):
 
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'recipe')
+        read_only_fields = ('recipe',)
 
 
 class InstructionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     # What is the meta class
     class Meta:
         model = Instruction
         fields = ('id', 'name', 'recipe')
-        # read_only_fields = ('recipe',)
+        read_only_fields = ('recipe',)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -28,12 +33,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         # read_only_fields = ('recipe',)
 
     def create(self, validated_data):
-        print('create fn')
-        print('from serializer', validated_data)
+        # print('create fn')
+        # print('from serializer', validated_data)
         instructions = validated_data.pop('instructions')
         ingredients = validated_data.pop('ingredients')
-        print(ingredients)
-        print(instructions)
 
         my_recipe = Recipe.objects.create(**validated_data)
 
@@ -45,3 +48,58 @@ class RecipeSerializer(serializers.ModelSerializer):
             Instruction.objects.create(**instruction, recipe=my_recipe)
 
         return my_recipe
+
+    def update(self, instance, validated_data):
+        instructions = validated_data.pop('instructions')
+        ingredients = validated_data.pop('ingredients')
+        
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.title)
+        instance.serving_size = validated_data.get('serving_size', instance.serving_size)
+        instance.save()
+
+        # Update ingredients
+        initial_ingredients = dict((ingredient.id, ingredient) for ingredient in instance.ingredients.all())
+        initial_instructions = dict((instruction.id, instruction) for instruction in instance.instructions.all())
+        remaining_ingredients = []
+        remaining_instructions = []
+        for ingredient in ingredients:
+            # check if its a new ingredient or not, a new ingredient doesn't have an id.
+            if "id" in ingredient.keys():
+                if Ingredient.objects.filter(id=ingredient["id"]).exists():
+                    ingredient_item = initial_ingredients.pop(ingredient["id"])
+                    ingredient_item.name = ingredient.get('name', ingredient_item.name)
+                    ingredient_item.save()
+                    remaining_ingredients.append(ingredient_item.id)
+                else:
+                    continue
+            # Implies this is a new ingredient
+            else:
+                ingredient_item = Ingredient.objects.create(**ingredient, recipe=instance)
+                remaining_ingredients.append(ingredient_item.id)
+
+        if len(initial_ingredients) > 0:
+            for ingredient in initial_ingredients.values():
+                ingredient.delete()
+     
+
+
+        for instruction in instructions:
+            if "id" in instruction.keys():
+                if Instruction.objects.filter(id=instruction["id"]).exists():
+                    instruction_item = initial_instructions.pop(instruction["id"])
+                    instruction_item.name = instruction.get('name', instruction_item.name)
+                    instruction_item.save()
+                    remaining_instructions.append(instruction_item.id)
+                else:
+                    continue
+            else:
+                Instruction.objects.create(**instruction, recipe=instance)
+                remaining_instructions.append(instruction_item.id)
+
+        if len(initial_instructions) > 0:
+            for instruction in initial_instructions.values():
+                instruction.delete()
+     
+
+        return instance
