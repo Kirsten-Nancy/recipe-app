6b528a65ab2ba087d78f8f30d44cf7f03337d721
Kirsten Nancy <korjnan@gmail.com>
2021-08-07 22:44:13 +0800

    added user field to Recipe model

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -3,13 +3,15 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from rest_framework import serializers
 from rest_framework.fields import ImageField
-
+from django.conf import settings
 # Create your models here.
 
 
 class Recipe(models.Model):
+    
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recipes')
     serving_size = models.IntegerField(default=0)
     image = models.ImageField(upload_to='images',blank=True,null=True)
 
