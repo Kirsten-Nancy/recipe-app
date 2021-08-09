e350545fced32434f4b93e0a3211181ab1854725
Kirsten Nancy <korjnan@gmail.com>
2021-08-09 22:28:04 +0800

    Added user app urls

--- backend/urls.py
+++ backend/urls.py
@@ -27,7 +27,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/recipes/', include('recipeapp.urls')),
+    path('api/recipes/', include('recipeapp.urls', namespace='recipes')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
