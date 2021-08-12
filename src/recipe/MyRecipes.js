6d602fc59bae360164bf7f409b56537f68a48f37
Kirsten Nancy <korjnan@gmail.com>
2021-08-12 22:57:28 +0800

    Added individual recipe page

---
+++ src/recipe/MyRecipes.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react"
+import axiosInstance from "../api"
+import Food from "./Food"
+
+const MyRecipes = () => {
+  const [data, setData] = useState([])
+
+  const fetchRecipes = () => {
+    axiosInstance.get("recipes/current-user/").then((response) => {
+      console.log("response", response)
+      console.log(response.data)
+      setData(response.data)
+    })
+  }
+
+  useEffect(() => {
+    fetchRecipes()
+  }, [])
+  return (
+    <div className="main-container">
+      {data.map((recipe, index) => {
+        return <Food key={index} recipe={recipe} />
+      })}
+    </div>
+  )
+}
+
+export default MyRecipes
